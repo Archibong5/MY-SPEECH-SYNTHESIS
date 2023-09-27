3f1d6b5665ded6f2c76598622ea366c9502614af
Archibong5 <109733824+Archibong5@users.noreply.github.com>
2023-09-27 02:32:18 +0100

    Add initial speech synthesis website file

---
+++ index.js
@@ -0,0 +1,43 @@
+const msg = new SpeechSynthesisUtterance();
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
+
+// msg.text = document.querySelector('[name="text"]');
+
+
+let voices = []
+
+function populateVoices(){
+  voices = this.getVoices();
+  const voiceOptions = voices
+              .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+              .join('');
+
+  voicesDropdown.innerHTML = voiceOptions;
+}
+
+
+function setVoice(){
+ msg.voice = voices.find(voice => voice.name === this.value);
+ toggle()
+}
+
+function setOption(){
+  msg[this.name] = this.value;
+  toggle()
+}
+
+function toggle(startOver){
+  speechSynthesis.cancel();
+  if(startOver){
+    speechSynthesis.speak(msg);
+  }
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices)
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption))
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', toggle.bind(null, false));
